67cadb78f4ab7f292ca090ef03bd322eb19da74f
nimeshkk <nimeshkliyanage@gmail.com>
2023-06-25 16:09:08 +0530

    Initialize project using Create React App new

---
+++ src/components/header/header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './headerCss.css'
+
+export default function header1() {
+  return (
+    <div className="container">
+        <div className="logo">
+            logo
+
+        </div>
+
+        <div className="right">
+
+            <div className="menu"></div>
+           <input type="text" className="search"></input>
+           <span>png</span>
+        </div>
+      
+    </div>
+  )
+}
+
