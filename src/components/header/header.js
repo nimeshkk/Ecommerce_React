3ac8a52edb9687e516d480893aa083e7b97a2116
nimeshkk <nimeshkliyanage@gmail.com>
2023-06-25 19:30:14 +0530

    update

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import './headerCss.css'
+import Logo from '../../assets/img/(11).png'
+import {FiShoppingCart} from 'react-icons/fi'
 
 export default function header1() {
   return (
     <div className="container">
+
         <div className="logo">
-            logo
+            <img src={Logo}></img>
+            <span>OrderSite</span>
 
         </div>
 
         <div className="right">
 
-            <div className="menu"></div>
-           <input type="text" className="search"></input>
-           <span>png</span>
+            <div className="menu">
+                <ul className="menu">
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Portfolio</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+
+           <input type="text" className="search" placeholder='search'></input>
+           <FiShoppingCart className="cart"/>
         </div>
       
     </div>
