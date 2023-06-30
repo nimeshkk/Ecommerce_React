38a92ff4d3f2d5a155032035459618e9a78dacb6
nimeshkk <nimeshkliyanage@gmail.com>
2023-07-01 00:30:50 +0530

    update

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -6,7 +6,11 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import SwiperCore, { Navigation } from 'swiper';
 
-import img from '../../assets/img/(11).png';
+
+import img2 from '../../assets/img/black-back-sweater.jpg';
+import img3 from '../../assets/img/closeup.jpg';
+import img4 from '../../assets/img/blue-t-shirt.jpg';
+import img5 from '../../assets/img/pink-sweater-front.jpg';
 
 SwiperCore.use([Navigation]);
 
@@ -24,21 +28,22 @@ export default function Slider() {
           onSlideChange={() => console.log('slide change')}
           onSwiper={(swiper) => console.log(swiper)}
         >
+        
           <SwiperSlide>
-            <img src={img} alt="Slide 1" className="slide-image" />
-            <div className="slide-description">Description 1</div>
+            <img src={img2} alt="Slide 1" className="slide-image" />
+            <div className="slide-description">black-back-sweater</div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={img} alt="Slide 2" className="slide-image" />
-            <div className="slide-description">Description 2</div>
+            <img src={img3} alt="Slide 2" className="slide-image" />
+            <div className="slide-description">Men's Denim</div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={img} alt="Slide 3" className="slide-image" />
-            <div className="slide-description">Description 3</div>
+            <img src={img4} alt="Slide 3" className="slide-image" />
+            <div className="slide-description">blue-t-shirt</div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={img} alt="Slide 4" className="slide-image" />
-            <div className="slide-description">Description 4</div>
+            <img src={img5} alt="Slide 4" className="slide-image" />
+            <div className="slide-description">pink-sweater</div>
           </SwiperSlide>
         </Swiper>
         
