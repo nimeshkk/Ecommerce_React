f8aa824f73418089963c139d98dc45ec1a4118bd
nimeshkk <nimeshkliyanage@gmail.com>
2023-06-30 14:10:00 +0530

    update

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,52 +1,31 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import './nav.css';
-import '../../components/nav/nav.css';
-import Logo from '../../assets/img/(11).png';
-import { FiShoppingCart } from 'react-icons/fi';
-import { LuAlignJustify } from 'react-icons/lu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function Nav() {
-  const [active, setActive] = useState(false);
+  const navRef = useRef(null);
 
-  const toggleMenu = () => {
-    setActive(!active);
+  const showNavbar = () => {
+    navRef.current.classList.toggle('responsive_nav');
   };
 
   return (
-    <div className="container">
-      <div className="logo">
-        <img src={Logo} alt="Logo" />
-        <span>OrderSite</span>
-      </div>
-
-      <div className="right">
-        <div className={`menu ${active ? 'active' : ''}`}>
-          <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Blog</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-
-        <input type="text" className="search" placeholder="Search" />
-        <FiShoppingCart className="cart" />
-        <LuAlignJustify className="menu-bars" onClick={toggleMenu} />
-
-        {active && (
-          <div className="dropdown-menu">
-            <ul>
-              <li>Home</li>
-              <li>Services</li>
-              <li>Portfolio</li>
-              <li>Blog</li>
-              <li>Contact</li>
-            </ul>
-          </div>
-        )}
-      </div>
-    </div>
+    <header>
+      <h3>logo</h3>
+      <nav ref={navRef}>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FontAwesomeIcon icon={faTimesCircle} />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+    </header>
   );
 }
+
 
